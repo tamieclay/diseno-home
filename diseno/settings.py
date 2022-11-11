fb974ec6a270b585b571c3e88dd00b35622e08f0
tamieclay <68555636+tamieclay@users.noreply.github.com>
2022-11-11 05:12:27 +0200

    Second commit

--- diseno/settings.py
+++ diseno/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-oeffaij#53wm*jdlvnz7$(wjj3qhd7-^bi_ktqnbr1%adz7j-y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://store.disenoenergy.com/",'https://home.disenoenergy.com/']
+ALLOWED_HOSTS = ['https://store.disenoenergy.com/','https://home.disenoenergy.com/','home.disenoenergy.com']
 
 
 # Application definition
